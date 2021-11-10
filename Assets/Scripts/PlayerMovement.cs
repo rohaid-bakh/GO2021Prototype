22a003633d51bc93f4fa844129c3723c98985145
rohaid-bakh <rohaidb1999@gmail.com>
2021-11-09 23:57:57 -0500

    Prototype 1 added catch functionality to run, jump, walk. Created an additional map to test. Organized between 3 different files.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 public class PlayerMovement : MonoBehaviour
 {
-  
+
     public CharacterController2D script;
 
     float horizontalAxis = 0f; // x input
@@ -13,33 +13,52 @@ public class PlayerMovement : MonoBehaviour
     bool jump = false;
     bool crouch = false;
     bool run = false;
+    bool catchBug = false;
 
 
-        void Update()
+    void Update()
     {
-        
-        horizontalAxis = Input.GetAxisRaw("Horizontal") * walkSpeed ;
 
-        if (Input.GetButtonDown("Jump")) {
+        horizontalAxis = Input.GetAxisRaw("Horizontal") * walkSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
             jump = true;
         }
-        if (Input.GetButtonDown("Crouch")) {
+        if (Input.GetButtonDown("Crouch"))
+        {
             crouch = true;
-        } else if (Input.GetButtonUp("Crouch")) {
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
             crouch = false;
         }
-        
-        if (Input.GetButtonDown("Run")) {
+
+        if (Input.GetButtonDown("Run"))
+        {
             run = true;
-        } else if (Input.GetButtonUp("Run")) {
+        }
+        else if (Input.GetButtonUp("Run"))
+        {
             run = false;
         }
 
-      
-    } 
+        if (Input.GetButtonDown("Catch"))
+        {
+            catchBug = true;
+        }
 
-    void FixedUpdate() {
-        script.Move(horizontalAxis * Time.fixedDeltaTime, crouch, jump, run);
-        jump = false;
+
     }
+
+  
+
+    void FixedUpdate()
+    {
+        script.Move(horizontalAxis * Time.fixedDeltaTime, crouch, jump, run, catchBug);
+        jump = false;
+        catchBug = false;
+    }
+    
+    
 }
